5d98d9449d87bace9a475d2567a3dacfa4d029f5
Tales Gomes <Tales Gomes>
2017-03-12 15:54:32 -0300

    minor edits

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 
-import { NavController, IonicModule } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
-  items
+  items:[{title:string,checked:string}];
   constructor(public navCtrl: NavController) {
     
     
     this.items = [
-        {title: 'item1'},
-        {title: 'item2'},
-        {title: 'item3'},
-        {title: 'item4'},
-        {title: 'item5'},
-        {title: 'item6'}
+        {title: 'item1',checked:'checkmark-circle-outline'},
+        {title: 'item2',checked:'checkmark-circle-outline'},
+        {title: 'item3',checked:'checkmark-circle-outline'},
+        {title: 'item4',checked:'checkmark-circle-outline'},
+        {title: 'item5',checked:'checkmark-circle-outline'},
+        {title: 'item6',checked:'checkmark-circle-outline'}
     ];
   }
     removeItem(item){
@@ -26,10 +26,24 @@ export class Page1 {
  
       if(this.items[i] == item){
         this.items.splice(i, 1);
+          console.log(item.checked);
       }
  
     }
+  }
+  checkItem(item){
+     let i:number;
+    for(i = 0; i < this.items.length; i++) {
  
+      if(this.items[i] == item){
+        if(item.checked == 'checkmark-circle-outline'){
+        this.items[i].checked ='checkmark-circle';
+    }else{
+      this.items[i].checked='checkmark-circle-outline';
+    }
+      }
+ 
+    }
   }
 
 }
