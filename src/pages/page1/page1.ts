86e4f88ddf68619ba611ca0ecde756c7bd464fcf
teious <talesgp_96@hotmail.com.br>
2017-06-21 23:57:37 -0300

    final

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit} from '@angular/core';
-import { NavController, ModalController} from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
 import * as moment from 'moment';
-import{ReceitaForm} from './receita-form';
-import{DespesaForm} from './despesa-form';
-import {TransactionService} from '../../providers/transaction';
-import {Transaction} from '../../models/transaction';
+import { ReceitaForm } from './receita-form';
+import { DespesaForm } from './despesa-form';
+import { TransactionService } from '../../providers/transaction';
+import { Transaction } from '../../models/transaction';
 import 'moment/src/locale/pt-br';
 import 'moment/locale/pt-br';
 
@@ -14,155 +14,179 @@ import 'moment/locale/pt-br';
 })
 
 export class Page1 implements OnInit {
-  tipoSaldo:String;
-  items:Transaction[];
-  month:string;
-  monthStart:String;
-  monthEnd:String;
-  t:any;
-  saldoTotal:number;
-  saldoMensal:number;
-  allItems:Transaction[];
-  
-  constructor(public navCtrl: NavController, public modalCtrl:ModalController, public transactionService:TransactionService) { 
-    this.tipoSaldo= "mensal"
+  loading = false;
+  tipoSaldo: String;
+  items: Transaction[];
+  month: string;
+  monthStart: String;
+  monthEnd: String;
+  t: any;
+  saldoTotal: number;
+  saldoMensal: number;
+  allItems: Transaction[];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public transactionService: TransactionService) {
+    this.tipoSaldo = "mensal"
     this.saldoTotal = 0;
     this.saldoMensal = 0;
     this.t = moment();
-    this.t.locale('pt-br'); 
-    this.month = this.t.format('MMMM, YYYY') ;
-
-  }
-  ngOnInit(){
-   this.transactionService.getTransactions()
-   .then((value:Transaction[]) =>{
-    this.allItems = value;
-      for(let transaction of this.allItems){
-        if(transaction.checked)this.saldoTotal+=transaction.value;
-      }
-   });
-
-   this.monthStart = this.t.startOf('month').toISOString() ;
-   this.monthEnd = this.t.endOf('month').subtract(1,'days').toISOString();
-
-   this.transactionService
-   .getTransactionsByDate(this.monthStart, this.monthEnd)
-   .then((value:Transaction[])=> {
-    this.items =value;
-       for(let item of this.items) {
-         if(item.checked)this.saldoMensal += item.value;
-         }
-   });  
-  }
-
-
-  removeItem(item){
-
- 
-        if(item.checked){
-          this.saldoMensal -= item.value;       
-          this.saldoTotal -= item.value;
-        }
-        this.transactionService.deleteTransaction(item);
-
-     
-      
-  
-
-  }
-  checkItem(item){
-
-      if(item.checked){
-            item.checked=false;
-            this.saldoMensal -= item.value;   
-            this.saldoTotal -= item.value;
-      }else{
-            item.checked=true;
-            this.saldoMensal += item.value;
-            this.saldoTotal += item.value;
-      }           
-      
-      this.transactionService
-      .updateTransaction(item);  
-  }
-
-  prevMonth(){
-    
-      this.t.subtract(1,'months');
-      this.month = this.t.format('MMMM, YYYY') ;
-      this.monthStart = this.t.startOf('month').toISOString() ;
-      this.monthEnd = this.t.endOf('month').subtract(1,'days').toISOString();
-
-      this.transactionService
-      .getTransactionsByDate(this.monthStart, this.monthEnd)
-      .then((value: Transaction[])=> {
-
-      this.items = value;
-      this.saldoMensal = 0;
-      
-      for(let item of  this.items) {
-        if(item.checked)this.saldoMensal +=item.value;
-      }
-
-    });
-
-  }
-
- nextMonth(){
-
-    this.t.add(1,'months');
+    this.t.locale('pt-br');
     this.month = this.t.format('MMMM, YYYY');
-    this.monthStart = this.t.startOf('month').toISOString() ;
-    this.monthEnd = this.t.endOf('month').subtract(1,'days').toISOString();
+
+  }
+  ngOnInit() {
+    this.transactionService.getTransactions()
+      .then((value: Transaction[]) => {
+        this.allItems = value;
+        for (let transaction of this.allItems) {
+          if (transaction.checked) this.saldoTotal += transaction.value;
+        }
+      });
+
+    this.monthStart = this.t.startOf('month').toISOString();
+    this.monthEnd = this.t.endOf('month').subtract(1, 'days').toISOString();
 
     this.transactionService
-    .getTransactionsByDate(this.monthStart, this.monthEnd)
-    .then((value: Transaction[])=> {
+      .getTransactionsByDate(this.monthStart, this.monthEnd)
+      .then((value: Transaction[]) => {
+        this.items = value;
+        for (let item of this.items) {
+          if (item.checked) this.saldoMensal += item.value;
+        }
+      });
+  }
 
-    this.items = value;
-    this.saldoMensal = 0;
 
-    for(let item of this.items) {
-      if(item.checked)this.saldoMensal +=item.value;
+  removeItem(item) {
+
+
+    if (item.checked) {
+      this.saldoMensal -= item.value;
+      this.saldoTotal -= item.value;
+    }
+    this.transactionService.deleteTransaction(item);
+
+
+
+
+
+  }
+  checkItem(item) {
+
+    if (item.checked) {
+      item.checked = false;
+      this.saldoMensal -= item.value;
+      this.saldoTotal -= item.value;
+    } else {
+      item.checked = true;
+      this.saldoMensal += item.value;
+      this.saldoTotal += item.value;
     }
 
-   });
-
-}
-
-addDespesa(){
-
-  let modal = this.modalCtrl.create(DespesaForm);
-  modal.present();
-  modal.onDidDismiss(()=>{
     this.transactionService
-    .getTransactionsByDate(this.monthStart, this.monthEnd)
-    .then((value: Transaction[])=> {
+      .updateTransaction(item);
+  }
+
+  prevMonth() {
+
+    this.t.subtract(1, 'months');
+    this.month = this.t.format('MMMM, YYYY');
+    this.monthStart = this.t.startOf('month').toISOString();
+    this.monthEnd = this.t.endOf('month').subtract(1, 'days').toISOString();
+
+    this.transactionService
+      .getTransactionsByDate(this.monthStart, this.monthEnd)
+      .then((value: Transaction[]) => {
+
         this.items = value;
         this.saldoMensal = 0;
-        for(let item of this.items) {
-          if(item.checked)this.saldoMensal += item.value;
+
+        for (let item of this.items) {
+          if (item.checked) this.saldoMensal += item.value;
         }
-   });
-    
-    });
-}
 
-addReceita(){
+      });
 
-  let modal = this.modalCtrl.create(ReceitaForm);
-  modal.present();
-  modal.onDidDismiss(()=>{
+  }
+
+  nextMonth() {
+
+    this.t.add(1, 'months');
+    this.month = this.t.format('MMMM, YYYY');
+    this.monthStart = this.t.startOf('month').toISOString();
+    this.monthEnd = this.t.endOf('month').subtract(1, 'days').toISOString();
+
     this.transactionService
-    .getTransactionsByDate(this.monthStart, this.monthEnd)
-    .then((value: Transaction[])=> {
+      .getTransactionsByDate(this.monthStart, this.monthEnd)
+      .then((value: Transaction[]) => {
+
         this.items = value;
         this.saldoMensal = 0;
-        for(let item of this.items) {
-          if(item.checked)this.saldoMensal += item.value;
+
+        for (let item of this.items) {
+          if (item.checked) this.saldoMensal += item.value;
         }
-   });
-    
+
+      });
+
+  }
+
+  addDespesa() {
+
+    let modal = this.modalCtrl.create(DespesaForm);
+    modal.present();
+    modal.onDidDismiss(() => {
+      this.transactionService
+        .getTransactionsByDate(this.monthStart, this.monthEnd)
+        .then((value: Transaction[]) => {
+          this.items = value;
+          this.saldoMensal = 0;
+          for (let item of this.items) {
+            if (item.checked) this.saldoMensal += item.value;
+          }
+        });
+
     });
-}
+  }
+
+  addReceita() {
+
+    let modal = this.modalCtrl.create(ReceitaForm);
+    modal.present();
+    modal.onDidDismiss(() => {
+      this.transactionService
+        .getTransactionsByDate(this.monthStart, this.monthEnd)
+        .then((value: Transaction[]) => {
+          this.items = value;
+          this.saldoMensal = 0;
+          for (let item of this.items) {
+            if (item.checked) this.saldoMensal += item.value;
+          }
+        });
+
+    });
+  }
+
+  refresh() {
+    this.loading = true;
+    this.transactionService.getTransactions()
+      .then((value: Transaction[]) => {
+        this.allItems = value;
+        this.saldoTotal = 0;
+        for (let transaction of this.allItems) {
+          if (transaction.checked) this.saldoTotal += transaction.value;
+        }
+      });
+    this.transactionService
+      .getTransactionsByDate(this.monthStart, this.monthEnd)
+      .then((value: Transaction[]) => {
+        this.items = value;
+        this.saldoMensal = 0;
+        for (let item of this.items) {
+          if (item.checked) this.saldoMensal += item.value;
+        }
+        this.loading = false;
+      });
+
+  }
 }
--- src/providers/transaction.ts
+++ src/providers/transaction.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import PouchDB from 'pouchdb';
-import {Transaction} from '../models/transaction'
+import { Transaction } from '../models/transaction'
 
 /*
   Generated class for the Transactions provider.
@@ -15,127 +15,133 @@ import {Transaction} from '../models/transaction'
 export class TransactionService {
 
   data: any;
-  db:any;
-  remote:any;
+  db: any;
+  remote: any;
   databydate: any;
-  
+  username: any;
+  password: any;
 
   constructor(public http: Http) {
-      this.db = new PouchDB('finmoney');
-      this.remote = 'http://localhost:5984/finmoney';
+    this.db = new PouchDB('finmoney');
+    this.remote = 'https://5c756a36-cff1-4152-97ec-a762859edac0-bluemix.cloudant.com/finmoney';
+    this.username = "5c756a36-cff1-4152-97ec-a762859edac0-bluemix";
+    this.password = "d9375c4eb177670ca4d20ccd8d8d08b0628e70ad2a2f68c04e632e26d5fb44e6";
+    let options = {
+      live: true,
+      retry: true,
+      continuous: true,
+      auth: {
+        username: this.username,
+        password: this.password
+      }
+    };
+    this.db.sync(this.remote, options);
+  }
+  getTransactionsByDate(startKey: String, endKey: String) {
 
-      let options = {
-        live: true,
-        retry: true,
-        continuous: true
-      };
-      this.db.sync(this.remote, options);
-}
-getTransactionsByDate(startKey:String, endKey:String){
+    return new Promise(resolve => {
 
-  return new Promise(resolve => {
- 
-    this.db.allDocs({
-      startkey: startKey,
-      endkey: endKey,
-      include_docs: true
- 
-    }).then((result) => {
- 
+      this.db.allDocs({
+        startkey: startKey,
+        endkey: endKey,
+        include_docs: true
+
+      }).then((result) => {
+
         this.data = [];
         result.rows.map((row) => {
-        this.data.push(row.doc);
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+      }).catch((error) => {
+
+        console.log(error);
+
       });
- 
-      resolve(this.data);
- 
-    }).catch((error) => {
- 
-      console.log(error);
- 
-    }); 
- 
-  });
-}
-getTransactions(){
- if (this.data) {
-    return Promise.resolve(this.data);
-     
+
+    });
+  }
+  getTransactions() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+
+    }
+
+    return new Promise(resolve => {
+
+      this.db.allDocs({
+        include_docs: true
+
+      }).then((result) => {
+
+        this.data = [];
+
+        result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({ live: true, since: 'now', include_docs: true }).on('change', (change) => {
+          this.handleChange(change);
+        });
+
+      }).catch((error) => {
+
+        console.log(error);
+
+      });
+
+    });
+  }
+  createTransaction(transaction: Transaction) {
+    this.db.post(transaction);
+  }
+  updateTransaction(transaction: Transaction) {
+    this.db.put(transaction).catch((err) => {
+      console.log(err);
+    });
   }
 
-  return new Promise(resolve => {
- 
-    this.db.allDocs({
-      include_docs: true
- 
-    }).then((result) => {
- 
-        this.data = [];
- 
-        result.rows.map((row) => {
-        this.data.push(row.doc);
-      });
- 
-      resolve(this.data);
- 
-      this.db.changes({live: true, since: 'now', include_docs: true}).on('change', (change) => {
-        this.handleChange(change);
-      });
- 
-    }).catch((error) => {
- 
-      console.log(error);
- 
-    }); 
- 
-  });
-}
-createTransaction(transaction:Transaction){
-  this.db.post(transaction);
-}
-updateTransaction(transaction: Transaction){
- this.db.put(transaction).catch((err)  =>{
-    console.log(err);
- });
-}
- 
-deleteTransaction(transaction){
-  this.db.remove(transaction).catch((err)=>{
-  console.log(err);
- });
-}
- 
-handleChange(change){
-  let changedDoc = null;
-  let changedIndex = null;
- 
-  this.data.forEach((doc, index) => {
- 
-    if(doc._id === change.id){
-      changedDoc = doc;
-      changedIndex = index;
+  deleteTransaction(transaction) {
+    this.db.remove(transaction).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  handleChange(change) {
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+
+    });
+
+    //A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
     }
- 
-  });
- 
-  //A document was deleted
-  if(change.deleted){
-    this.data.splice(changedIndex, 1);
-  } 
-  else {
- 
-    //A document was updated
-    if(changedDoc){
-      this.data[changedIndex] = change.doc;
-    } 
- 
-    //A document was added
     else {
-      this.data.push(change.doc); 
+
+      //A document was updated
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      }
+
+      //A document was added
+      else {
+        this.data.push(change.doc);
+      }
+
     }
- 
+
   }
- 
-}
 
 }
